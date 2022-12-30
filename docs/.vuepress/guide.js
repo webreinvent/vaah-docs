98058132b3baf1180a9ec3cf6356fab5d09fce95
nitin kumar <nitin.k001@webinvent.com>
2022-12-30 16:57:47 +0530

    Added: generate module

--- docs/.vuepress/guide.js
+++ docs/.vuepress/guide.js
@@ -19,6 +19,10 @@ sidebar = [
         text: 'Laravel',
         link: '/guide/laravel.md',
     },
+    {
+        text: 'Generate Module',
+        link: '/guide/generate-module.md',
+    },
 
 ];
 
