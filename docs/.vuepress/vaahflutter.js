bea49937744f5b414894425eb52ef672280a51ee
Prajapati Chintan <prajapati.c001@webreinvent.com>
2023-01-09 20:22:53 +0530

    Updated: directory_structure/vaahextendflutter/widgets

--- docs/.vuepress/vaahflutter.js
+++ docs/.vuepress/vaahflutter.js
@@ -103,6 +103,38 @@ sidebar = [
                             { text: 'api self sign', link: '/vaahflutter/directory_structure/vaahextendflutter/services/self-sign.md' },
                         ],
                     },
+                    {
+                        text: 'widgets',
+                        collapsible: true,
+                        children: [
+                            {
+                                text: 'atoms',
+                                collapsible: true,
+                                children: [
+                                    { text: 'container with rounded corners', link: '/vaahflutter/directory_structure/vaahextendflutter/widgets/atoms/container-with-rounded-corners.md' },
+                                    { text: 'expansion panel', link: '/vaahflutter/directory_structure/vaahextendflutter/widgets/atoms/expansion-panel.md' },
+                                    { text: 'inputs', link: '/vaahflutter/directory_structure/vaahextendflutter/widgets/atoms/inputs.md' },
+                                    { text: 'auto complete input', link: '/vaahflutter/directory_structure/vaahextendflutter/widgets/atoms/auto-complete-input.md' },
+                                    { text: 'date time input', link: '/vaahflutter/directory_structure/vaahextendflutter/widgets/atoms/date-time-input.md' },
+                                    { text: 'file picker', link: '/vaahflutter/directory_structure/vaahextendflutter/widgets/atoms/file-picker.md' },
+                                    { text: 'slider input', link: '/vaahflutter/directory_structure/vaahextendflutter/widgets/atoms/slider-input.md' },
+                                ],
+                            },
+                            {
+                                text: 'molecules',
+                                collapsible: true,
+                                children: [
+                                ],
+                            },
+                            {
+                                text: 'organisms',
+                                collapsible: true,
+                                children: [
+                                ],
+                            },
+                        ],
+                    },
+                    { text: 'docs', link: '/vaahflutter/directory_structure/vaahextendflutter/docs.md' },
                 ]
             },
         ]
