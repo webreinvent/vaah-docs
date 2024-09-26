7100e5b5c980e042ec2c81af3daf3c29e0dc786d
Shivam Gupta <shivam-g001@webreinvent.com>
2024-09-26 14:14:59 +0530

    Updated: docs with module scalar api-reference

--- app.vue
+++ app.vue
@@ -67,6 +67,12 @@ const links = computed(() => {
       active: route.path.startsWith('/vaahstore')
     },
     {
+      label: 'VaahStore APIs',
+      icon: 'i-heroicons-rocket-launch',
+      to: '/api-docs',
+      active: route.path.startsWith('/docs')
+    },
+    {
       label: 'VaahShare',
       icon: 'i-heroicons-rocket-launch',
       to: '/vaahshare',
---
+++ pages/api-docs.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ApiReference } from '@scalar/api-reference'
+import '@scalar/api-reference/style.css'
+
+
+let apiConfig;
+
+
+if (process.client) {
+  const baseUrl = window.location.origin; // Get the current origin
+
+  // Construct the full URL for the OpenAPI spec file
+  apiConfig = {
+    spec: {
+      url: `${baseUrl}/vaahstore.yaml`, // Path to your OpenAPI spec file
+    },
+  };
+}
+</script>
+
+<template>
+  <!-- Only render the ApiReference if apiConfig is defined -->
+  <ApiReference v-if="apiConfig" :configuration="apiConfig" />
+</template>
