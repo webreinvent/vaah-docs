0fe1a0190d3fe83654ff2e7db0a80bb2a825e3c6
Chintan Prajapati <prajapati.c001@webreinvent.com>
2024-10-23 17:40:43 +0530

    Fixed: sidebar indexing levels

--- layouts/docs.vue
+++ layouts/docs.vue
@@ -1,27 +1,26 @@
 <script setup lang="ts">
-import type { NavItem } from '@nuxt/content/dist/runtime/types'
+import type { NavItem } from "@nuxt/content/dist/runtime/types";
 import BranchSelect from "../components/BranchSelect.vue";
-const route = useRoute()
+const route = useRoute();
 
-const nav = inject<Ref<NavItem[]>>('navigation')
+const nav = inject<Ref<NavItem[]>>("navigation");
 
 const navigation = computed(() => {
-  const n =  nav.value.filter(item => {
-    return item._path.startsWith('/' + route.path.split("/")[1])
-  })
+  const n = nav.value.filter((item) => {
+    return item._path.startsWith("/" + route.path.split("/")[1]);
+  })[0]["children"];
 
-  if(n.at(0)?.children) {
+  if (n.at(0)?.children) {
     return n.at(0)?.children.map((item) => {
-      if(item._path === '/vaahstore/api') {
-        return {...item, _path: '/vaahstore/api'}
+      if (item._path === "/vaahstore/api") {
+        return { ...item, _path: "/vaahstore/api" };
       }
-      return item
-    })
+      return item;
+    });
   } else {
     return n;
   }
-})
-
+});
 </script>
 
 <template>
